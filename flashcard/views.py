6889586d9d6151855efa80eed89e0b2e8038e97d
Hugo Arcanjo <hfarcanjo@gmail.com>
2024-01-18 18:59:52 -0300

    feat: register flashcard from view

--- flashcard/views.py
+++ flashcard/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Categoria, Flashcard
+from django.http import HttpResponse
+from django.contrib.messages import constants
+from django.contrib import messages
 
 # Create your views here.
+def novo_flashcard(request):
+    if not request.user.is_authenticated:
+        return redirect('/usuarios/login')
+
+    if request.method == 'GET':
+        categorias = Categoria.objects.all()
+        dificuldades = Flashcard.DIFICULDADE_CHOICES
+
+        return render(
+            request,
+            'novo_flashcard.html',
+            {
+                'categorias': categorias,
+                'dificuldades': dificuldades,
+            }
+            
+        )
+    elif request.method == 'POST':
+        pergunta = request.POST.get('pergunta')
+        resposta = request.POST.get('resposta')
+        categoria = request.POST.get('categoria')
+        dificuldade = request.POST.get('dificuldade')
+
+        if len(pergunta.strip()) == 0 or len(resposta.strip()) == 0:
+            messages.add_message(
+                request,
+                constants.ERROR,
+                'Preencha os campos de pergunta e resposta',
+            )
+            return redirect('/flashcard/novo_flashcard')
+
+        flashcard = Flashcard(
+            user=request.user,
+            pergunta=pergunta,
+            resposta=resposta,
+            categoria_id=categoria,
+            dificuldade=dificuldade,
+        )
+
+        flashcard.save()
+
+        messages.add_message(
+            request, constants.SUCCESS, 'Flashcard criado com sucesso'
+        )
+        return redirect('/flashcard/novo_flashcard')
